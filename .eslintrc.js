a7ca8ea5259d55c8bb91813232b68403a2260ef3
Chuck LeDuc Díaz <chuck@caelumlabs.com>
2020-03-12 13:09:01 +0100

    fix build, add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,26 @@
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es6: true,
-      mocha: true,
-    },
-    extends: [
-      'standard'
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly'
-    },
-    parserOptions: {
-      ecmaVersion: 2018
-    },
-    rules: {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    mocha: true,
+  },
+  extends: [
+    'standard',
+    'plugin:chai-friendly/recommended',
+    'plugin:jsdoc/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  plugins: [
+    'chai-friendly',
+    'jsdoc'
+  ],
+  rules: {
   }
-  
+}
